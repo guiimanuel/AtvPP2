f852972f5eceae0ae3efb9d123019e58f30e98c4
Guilherme Manuel <129343802+guiimanuel@users.noreply.github.com>
2025-04-09 15:37:07 -0300

    Add files via upload

---
+++ Exercícios PP2 python/Exercícios.py
@@ -0,0 +1,100 @@
+#1 questao abaixo:
+#nome= input("Digite seu nome:")
+#print(nome + " é belo")
+
+#2 questao abaixo:
+#numero1 = int(input("Digite o primeiro número: "))
+#numero2 = int(input("Digite o segundo numero: "))
+#soma = numero1 + numero2
+#print("A soma dos números é igual a: " + str(soma))
+
+#3 questao abaixo:
+#preco = int(input("Digite o preço do produto: "))
+#qtd = int(input("Digite a quantidade do produto: "))
+#vt= preco*qtd
+#print("O valor total da compra é: R$" +str(vt))
+
+#4 questao abaixo:
+#preco = int(input("Digite o preço do produto: "))
+#desconto = float(input("Digite o percentual do desconto: "))
+#vf= preco*desconto
+#print("O preço com desconto é: R$"+ str(preco-vf))
+
+#5 questao abaixo:
+#salario = int(input("Digite o seu salário: "))
+#aumento = float(input("Digite o percentual do aumento: "))
+#sf= salario*aumento
+#print("O seu salário é: R$"+ str(salario+sf))
+
+#6 questao abaixo:
+#v1 = int(28)
+#v2 = int(43)
+#mult = v1*v2
+#print("O produto entre 28 e 43 é: " +str(mult))
+
+#7 questao abaixo:
+#n1 = int(input("Digite o primeiro número: "))
+#n2 = int(input("Digite o segundo numero: "))
+#n3 = int(input("Digite o terceiro número: "))
+#ma = (n1+n2+n3)/3
+#print("A média aritmética entre os números é: %.1f " % (ma))
+
+#8 questao abaixo:
+#numero = int(input("Digite um número: "))
+#ant = numero - 1
+#suc = numero + 1
+#print("O antecessor do número é "+str(ant)+" e o sucessor do numero é "+str(suc))
+
+#9 questao abaixo:
+#n1 = int(input("Digite o primeiro número: "))
+#n2 = int(input("Digite o segundo numero: "))
+#n3 = int(input("Digite o terceiro número: "))
+#n4 = int(input("Digite o quarto numero: "))
+#mp = (n1*1+n2*2+n3*3+n4*4)/(1+2+3+4)
+#print("A média ponderada dos números é: "+str(mp))
+
+#10 questao abaixo:
+#s = int(input("Digite o saldo: "))
+#sn = s*(2.5/100)
+#sf = s+sn
+#print("O novo saldo é: "+str(sf))
+
+#11 questao abaixo:
+#valor = int(input('Digite o valor: '))
+#tempo = int(input("Digite o tempo: "))
+#taxa = int(input('Digite o valor da taxa: '))
+#prestacao = valor+(valor*(taxa/100)*tempo)
+#print("O valor da prestacao é de :"+str(prestacao))
+
+#12 questao abaixo:
+#n1 = int(input("Digite o primeiro número: "))
+#n2 = int(input("Digite o segundo numero: "))
+#soma = n1 + n2
+#print("A soma dos dois números é: "+str(soma))
+#multi = n1*n2
+#print("A multiplicacao dos dois numeros é: "+str(multi))
+#divisao = n1/n2
+#print("A divisao entre os numeros é: "+str(divisao))
+#resto = n1%n2
+#print('O resto entre os numeros é: '+str(resto))
+
+#13 questao abaixo:
+#base = int(input("Digite a base do retangulo: "))
+#altura = int(input("Digite a altura do retangulo: "))
+#p = (base*2)+(altura*2)
+#area = base*altura
+#print("O perímetro é : "+str(p)+" A área é: "+str(area))
+
+#14 questao abaixo: 
+#base = int(input("Digite a base do triangulo: "))
+#altura = int(input("Digite a altura do triangulo: "))
+#area = (base*altura)/2
+#print("A área do triangulo é: "+str(area))
+
+#15 questao abaixo: 
+pi = 3.14
+pi = float(pi)
+raio = int(input("Digite o raio do circulo: "))
+p = 2*pi*raio
+area = pi*(raio*raio)
+print("O perímetro é: "+str(p)+" A área é: "+str(area))
